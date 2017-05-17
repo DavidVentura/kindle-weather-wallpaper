67a97a2773d8683f73444587cb6fac0a35b6a8ab
David <davidventura27@gmail.com>
2017-05-17 20:56:56 -0300

    check for wifi and screen saver status before downloading and displaying the saver

--- kindle/fetch.sh
+++ kindle/fetch.sh
@@ -4,9 +4,43 @@ FILE="/mnt/us/tmp.png"
 HEADER=""
 HEADER="Host: weather.davidventura.com.ar"
 
-wget --header="$HEADER" -O "$FILE" "$SERVER"
 
+sleeping=$(lipc-get-prop com.lab126.powerd status | grep -c "Screen Saver")
 
-if [ $? -eq 0 ]; then
-	eips -f -g "$FILE"
+if [ "$sleeping" -ne 1 ]; then
+	echo "Not sleeping."
+	exit 2
 fi
+
+wifienable() {
+	lipc-set-prop com.lab126.cmd wirelessEnable 1
+}
+
+is_connected() {
+	echo "$(lipc-get-prop com.lab126.wifid cmState | grep -c CONNECTED)";
+}
+
+wifienable
+tries=0
+while [ "$(is_connected)" -eq 0 ] && [ "$tries" -lt 10 ]; do
+	echo 'sleeping for wifi...'
+	tries=$((tries+1))
+	sleep 1
+done
+
+if [ "$tries" -eq 10 ]; then
+	echo "Wifi is off."
+	exit 1
+fi
+
+echo 'wifi => Connected'
+#wget --header="$HEADER" -O "$FILE" "$SERVER"
+#Kindle's wget doesn't support --header
+curl -s --header "$HEADER" "$SERVER" >"$FILE"                                   
+
+if [ $? -ne 0 ]; then
+	exit $?
+fi
+
+
+eips -f -g "$FILE"
