6842cc14e8ae239ded804907bcc65d45d01a6b61
David <davidventura27@gmail.com>
2017-05-17 13:14:45 -0300

    add header support, fix path typo

--- kindle/fetch.sh
+++ kindle/fetch.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 SERVER="http://web.labs/tmp.png"
-FILE="/mnt/us/tmp.png "
-wget -O "$FILE" "$SERVER"
+FILE="/mnt/us/tmp.png"
+HEADER=""
+HEADER="Host: weather.davidventura.com.ar"
+
+wget --header="$HEADER" -O "$FILE" "$SERVER"
+
+
 if [ $? -eq 0 ]; then
 	eips -f -g "$FILE"
 fi
